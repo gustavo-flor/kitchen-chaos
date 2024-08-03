4efeafa5d4f3d8f2ad636dd487d54d4d12005283
Gustavo Flôr <ogustaflor@gmail.com>
2024-08-03 11:21:28 -0300

    :sparkles: Adding cutting recipe scriptable objects

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOs;
     
     public override void Interact(Player player)
     {
         if (!HasKitchenObject() && player.HasKitchenObject())
         {
-            player.GetKitchenObject().SetKitchenObjectParent(this);
+            var kitchenObject = player.GetKitchenObject();
+            if (HasRecipeWithInput(kitchenObject.GetKitchenObjectSO()))
+            {
+                kitchenObject.SetKitchenObjectParent(this);
+            }
         }
         else if (HasKitchenObject() && !player.HasKitchenObject())
         {
@@ -22,7 +26,29 @@ public class CuttingCounter : BaseCounter
         {
             return;
         }
-        GetKitchenObject().DestroySelf();
-        KitchenObject.Spawn(cutKitchenObjectSO, this);
+        var kitchenObject = GetKitchenObject();
+        var output = GetOutputForInput(kitchenObject.GetKitchenObjectSO());
+        if (output != null)
+        {
+            kitchenObject.DestroySelf();
+            KitchenObject.Spawn(output, this);
+        }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO kitchenObject)
+    {
+        return GetOutputForInput(kitchenObject) != null;
+    }
+
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO input)
+    {
+        foreach (var cuttingRecipe in cuttingRecipeSOs)
+        {
+            if (cuttingRecipe.input == input)
+            {
+                return cuttingRecipe.output;
+            }
+        }
+        return null;
     }
 }
